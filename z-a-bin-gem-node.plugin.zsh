fa57aef12edb8cfbc8c97366d0c4a33edf6407b5
Salvydas Lukosius <sall@w-ss.io>
2022-04-06 12:27:28 +0100

    Update z-a-bin-gem-node.plugin.zsh

--- z-a-bin-gem-node.plugin.zsh
+++ z-a-bin-gem-node.plugin.zsh
@@ -3,9 +3,8 @@
 
 # According to the Zsh Plugin Standard:
 # https://z.digitalclouds.dev/community/zsh_plugin_standard/#the-proposed-function-name-prefixes
-0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
-
 # https://z.digitalclouds.dev/community/zsh_plugin_standard/#funtions-directory
 if [[ $PMSPEC != *f* ]] {
   fpath+=( "${0:h}/functions" )
