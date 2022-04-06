59946407db425e6fac60e5397509ed8a1eaa1933
Salvydas Lukosius <sall@w-ss.io>
2022-04-06 12:29:21 +0100

    Update z-a-bin-gem-node.plugin.zsh

--- z-a-bin-gem-node.plugin.zsh
+++ z-a-bin-gem-node.plugin.zsh
@@ -2,14 +2,16 @@
 # Copyright (c) 2021 Z-Shell ZI Contributors
 
 # According to the Zsh Plugin Standard:
-# https://z.digitalclouds.dev/community/zsh_plugin_standard/#the-proposed-function-name-prefixes
+# https://z.digitalclouds.dev/community/zsh_plugin_standard/#zero-handling
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
+
 # https://z.digitalclouds.dev/community/zsh_plugin_standard/#funtions-directory
 if [[ $PMSPEC != *f* ]] {
   fpath+=( "${0:h}/functions" )
 }
 
+# https://z.digitalclouds.dev/community/zsh_plugin_standard/#the-proposed-function-name-prefixes
 autoload .za-bgn-bin-or-src-function-body \
 .za-bgn-bin-or-src-function-body-cygwin \
 .za-bgn-mod-function-body \
