f29dfe5bcd3e726c2d468bfc92b4eabbdf6c9d04
Salvydas Lukosius <sall@w-ss.io>
2022-07-30 01:47:22 +0100

    [maintenance]: Repositroy files maintenace / Minor refactor 	new file:   .github/dependabot.yml 	deleted:    .github/renovate.json 	modified:   .github/workflows/trunk-check.yml 	modified:   .trunk/trunk.yaml 	modified:   LICENSE 	modified:   z-a-bin-gem-node.plugin.zsh

--- z-a-bin-gem-node.plugin.zsh
+++ z-a-bin-gem-node.plugin.zsh
@@ -1,9 +1,22 @@
 # -*- mode: zsh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
 # vim: ft=zsh sw=2 ts=2 et
 #
-# Copyright (c) 2021 Z-Shell Community
+#   Annex bin-gem-node | Copyright Ⓒ 2022 Z-Shell  Community
 #
-# According to the Zsh Plugin Standard:
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Zsh Plugin Standard:
 # https://z.digitalclouds.dev/community/zsh_plugin_standard/#zero-handling
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
@@ -13,34 +26,29 @@ if [[ $PMSPEC != *f* ]] {
   fpath+=( "${0:h}/functions" )
 }
 
-# https://z.digitalclouds.dev/community/zsh_plugin_standard/#the-proposed-function-name-prefixes
-#autoload -Uz .za-bgn-bin-or-src-function-body .za-bgn-bin-or-src-function-body-cygwin \
-#.za-bgn-mod-function-body →za-bgn-atload-handler →za-bgn-atclone-handler \
-#→za-bgn-atpull-handler →za-bgn-atdelete-handler \
-#→za-bgn-help-handler →za-bgn-shim-list
-
 # Autoload functions
 autoload -Uz "${0:h}/functions"/{.*,*}(.:t)
 
 # An empty stub to fill the help handler fields
 →za-bgn-null-handler() { :; }
 
-@zi-register-annex "z-a-bin-gem-node" subcommand:shim-list \
-  →za-bgn-shim-list \
-  →za-bgn-help-handler
+@zi-register-annex "z-a-bin-gem-node" \
+subcommand:shim-list \
+→za-bgn-shim-list \
+→za-bgn-help-handler
 
 @zi-register-annex "z-a-bin-gem-node" hook:\!atload-50 \
-  →za-bgn-atload-handler \
-  →za-bgn-null-handler "fbin''|sbin|sbin''|gem''|node''|pip''|fmod''|fsrc''|ferc''" # also register new ices
+→za-bgn-atload-handler \
+→za-bgn-null-handler "fbin''|sbin|sbin''|gem''|node''|pip''|fmod''|fsrc''|ferc''"
 
 @zi-register-annex "z-a-bin-gem-node" hook:atclone-50 \
-  →za-bgn-atclone-handler \
-  →za-bgn-null-handler
+→za-bgn-atclone-handler \
+→za-bgn-null-handler
 
 @zi-register-annex "z-a-bin-gem-node" hook:\%atpull-50 \
-  →za-bgn-atclone-handler \
-  →za-bgn-null-handler
+→za-bgn-atclone-handler \
+→za-bgn-null-handler
 
 @zi-register-annex "z-a-bin-gem-node" hook:atdelete-50 \
-  →za-bgn-atdelete-handler \
-  →za-bgn-null-handler
+→za-bgn-atdelete-handler \
+→za-bgn-null-handler
